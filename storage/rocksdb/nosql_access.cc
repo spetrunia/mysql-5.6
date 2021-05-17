af2c310e95343e4455c1cc3dcf893651d27e6c06
Sergei Petrunia <psergey@askmonty.org>
2021-05-17 18:15:37 +0300

    This adds a global my.cnf parameter, rocksdb_use_range_locking.
    (Cherry-picked on top of the pushed iterator patches)
    
    When it is ON, MyRocks will:
    - initialize RocksDB to use range-locking lock manager
    - for all DML operations (including SELECT .. FOR UPDATE) will lock
    the scanned range before reading/modifying rows.
    - In range locking mode, there is no snapshot checking (cannot do that
      for ranges). Instead, MyRocks will read and modify latest committed
      data, just like InnoDB does (in the code, grep for (start|end)
      _ignore_snapshot)
    - Queries that do not have a finite range to scan, like
      UPDATE t1 .... ORDER BY t1.key LIMIT n
      will use a  "Locking iterator" which will read rows, lock the range,
      and re-read the rows. See class LockingIterator.

--- storage/rocksdb/nosql_access.cc
+++ storage/rocksdb/nosql_access.cc
@@ -676,10 +676,11 @@ class select_exec {
     }
 
     rocksdb::Iterator *get_iterator(rocksdb::ColumnFamilyHandle *cf,
+                                    bool is_rev_cf,
                                     bool use_bloom,
                                     const rocksdb::Slice &lower_bound,
                                     const rocksdb::Slice &upper_bound) {
-      return rdb_tx_get_iterator(m_thd, cf, !use_bloom, lower_bound,
+      return rdb_tx_get_iterator(m_thd, cf, is_rev_cf, !use_bloom, lower_bound, //psergey-mergey-todo: or m_tx ?
                                  upper_bound, nullptr);
     }
 
@@ -1523,7 +1524,8 @@ bool INLINE_ATTR select_exec::setup_iterator(txn_wrapper *txn,
       m_thd, *m_key_def, eq_slice, bound_len, m_lower_bound_buf.data(),
       m_upper_bound_buf.data(), &m_lower_bound_slice, &m_upper_bound_slice);
   rocksdb::Iterator *it = txn->get_iterator(
-      m_key_def->get_cf(), use_bloom, m_lower_bound_slice, m_upper_bound_slice);
+      m_key_def->get_cf(), m_key_def->m_is_reverse_cf, use_bloom,
+      m_lower_bound_slice, m_upper_bound_slice);
   if (it == nullptr) {
     return true;
   }
